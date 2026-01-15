c81f6bed52e3203aec707c35525ec74f5b675e81
Jordan Castro <jordan@grupojvm.com>
2026-01-14 21:06:07 -0500

    Added license

--- src/python/module.rs
+++ src/python/module.rs
@@ -1,3 +1,11 @@
+// Copyright 2026 Jordan Castro <jordan@grupojvm.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
 use crate::{create_raw_string, free_raw_string, python::{add_new_name_idx_fn, exec_py, make_private, pocketpy, pocketpy_bridge, var_to_pocketpyref}, shared::{PtrMagic, module::Module, var::Var}};
 
 pub(super) fn create_module(module: &Module, parent: Option<&str>) {
--- src/python/var.rs
+++ src/python/var.rs
@@ -1,3 +1,11 @@
+// Copyright 2026 Jordan Castro <jordan@grupojvm.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
 use std::{ffi::c_void, sync::Arc};
 
 use crate::{borrow_string, create_raw_string, free_raw_string, python::{func::py_assign, object::create_object, pocketpy}, shared::{object::get_object, var::{Var, VarType}}};
